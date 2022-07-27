ae94570c04960fa7a34306581324c93586efc067
Rahim Ansari <rahimansari4560@gmail.com>
2022-07-27 12:51:04 +0530

    done

---
+++ my/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import New from './Components/New';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>React App Created</h1>
+      <New />
+    </div>
+  );
+}
+
+export default App;
